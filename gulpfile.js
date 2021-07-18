c7d2352ba0cfe2bafdebbb58a09f2ecd755fcec5
Yakov Danilichev <dan.yakov1105@mail.ru>
2021-07-18 21:48:05 +0300

    Fix styles + minor code style correction

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const {watch, series} = require('gulp');
 
-const styles = require('./tasks/scss.js');
-const clean = require('./tasks/clean.js')
+const styles = require('./tasks/scss');
+const clean = require('./tasks/clean');
 
 const watcher = () => {
   watch('src/scss/*.scss', {ignoreInitial: false}, styles);
